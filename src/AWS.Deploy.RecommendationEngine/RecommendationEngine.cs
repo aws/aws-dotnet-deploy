caf99b72d6127617f273af4586b84f8a654d97c1
Norm Johanson <normj@amazon.com>
2021-01-12 23:48:43 -0800

    Create JSON schema for recipe definitions and rework how recipe rules are defined.

--- src/AWS.Deploy.RecommendationEngine/RecommendationEngine.cs
+++ src/AWS.Deploy.RecommendationEngine/RecommendationEngine.cs
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -41,32 +42,113 @@ namespace AWS.Deploy.RecommendationEngine
 
             foreach (var potentialRecipe in _availableRecommendations)
             {
-                // If the recipe doesn't match on the required rules then skip
-                if (!projectDefinition.EvaluateRules(potentialRecipe.RecommendationRules.RequiredRules))
+                var results = EvaluateRules(projectDefinition, potentialRecipe.RecommendationRules);
+                if(!results.Include)
                 {
                     continue;
                 }
 
-                // If the recipe matches on the negative rules, meaning it has things in it that make it incompatible with the recipe, then skip.
-                if (potentialRecipe.RecommendationRules.NegativeRules?.Count > 0 && projectDefinition.EvaluateRules(potentialRecipe.RecommendationRules.NegativeRules))
-                {
-                    continue;
-                }
-
-                var priority = potentialRecipe.RecommendationRules.Priority;
-
-                // If it doesn't match on the optional rules it means we can most likely get it to work but it is not 
-                // the preferred approach so divide the priority in half.
-                if (!projectDefinition.EvaluateRules(potentialRecipe.RecommendationRules.OptionalRules))
-                {
-                    priority /= 2;
-                }
-
+                var priority = potentialRecipe.RecipePriority + results.PriorityAdjustment;
                 recommendations.Add(new Recommendation(potentialRecipe, projectDefinition.ProjectPath, priority));
             }
 
             recommendations = recommendations.OrderByDescending(recommendation => recommendation.ComputedPriority).ToList();
             return recommendations;
+        }
+
+        public RulesResult EvaluateRules(ProjectDefinition projectDefinition, IList<RecipeDefinition.RecommendationRuleItem> rules)
+        {
+            // If there are no rules the recipe must be invalid so don't include it.
+            if(false == rules?.Any())
+            {
+                return new RulesResult { Include = false };
+            }
+
+            var results = new RulesResult {Include = true };
+
+            foreach (var rule in rules)
+            {
+                var allTestPass = true;
+                foreach (var test in rule.Tests)
+                {
+                    switch (test.Type)
+                    {
+                        case "MSProjectSdkAttribute":
+                            allTestPass &= string.Equals(projectDefinition.SdkType, test.Condition.Value, StringComparison.InvariantCultureIgnoreCase);
+                            break;
+
+                        case "MSProperty":
+                            var propertyValue = projectDefinition.GetMSPropertyValue(test.Condition.PropertyName);
+                            allTestPass &= (propertyValue != null && test.Condition.AllowedValues.Contains(propertyValue));
+                            break;
+
+                        case "MSPropertyExists":
+                            allTestPass &= !string.IsNullOrEmpty(projectDefinition.GetMSPropertyValue(test.Condition.PropertyName));
+                            break;
+
+                        case "FileExists":
+                            var directory = Path.GetDirectoryName(projectDefinition.ProjectPath);
+                            allTestPass &= (Directory.GetFiles(directory, test.Condition.FileName).Length == 1);
+                            break;
+                        default:
+                            throw new InvalidRecipeDefinitionException($"Invalid test type for rule: {test.Type}");
+                    }
+
+                    if (!allTestPass)
+                        break;
+                }
+
+                results.Include &= ShouldInclude(rule.Effect, allTestPass);
+
+                var effectOptions = GetEffectOptions(rule.Effect, allTestPass);
+
+                if(effectOptions != null)
+                {
+                    if(effectOptions.PriorityAdjustment.HasValue)
+                    {
+                        results.PriorityAdjustment += effectOptions.PriorityAdjustment.Value;
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        public bool ShouldInclude(RecipeDefinition.RuleEffect effect, bool testPass)
+        {
+            // Get either the pass or fail effect options.
+            var effectOptions = GetEffectOptions(effect, testPass);
+            if (effectOptions != null)
+            {
+                if(effectOptions.Include.HasValue)
+                {
+                    return effectOptions.Include.Value;
+                }
+                else
+                {
+                    return true;
+                }
+                
+            }
+
+            return testPass;
+        }
+
+        private RecipeDefinition.EffectOptions GetEffectOptions(RecipeDefinition.RuleEffect effect, bool testPass)
+        {
+            if(effect == null)
+            {
+                return null;
+            }
+
+            return testPass ? effect.Pass : effect.Fail;
+        }
+
+        public class RulesResult
+        {
+            public bool Include { get; set; }
+
+            public int PriorityAdjustment { get; set; }
         }
     }
 }
