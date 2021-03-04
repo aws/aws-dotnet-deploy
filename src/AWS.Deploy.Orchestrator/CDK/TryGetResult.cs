512739de334bed2888a1844dd2253559a83049df
Ganesh Jangir <jangirg@amazon.com>
2021-03-03 21:32:39 -0800

    Remove \r

--- src/AWS.Deploy.Orchestrator/CDK/TryGetResult.cs
+++ src/AWS.Deploy.Orchestrator/CDK/TryGetResult.cs
@@ -1,4 +1,4 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.\r
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 namespace AWS.Deploy.Orchestrator.CDK
