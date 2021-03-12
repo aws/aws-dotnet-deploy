148ca055d480923d6ce56965fcadb3aa705d9813
philip pittle <pittle@amazon.com>
2021-03-12 14:23:40 -0800

    add samples that talk to a database

---
+++ testapps/ContosoUniversityBackendService/ContosoService.cs
@@ -0,0 +1,36 @@
+﻿using ContosoUniversityBackendService.Data;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ContosoUniversityBackendService
+{
+    public class ContosoService : BackgroundService
+    {
+        SchoolContext Context { get; }
+        public ContosoService(SchoolContext context)
+        {
+            Context = context;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Console.WriteLine("Student List");
+                Console.WriteLine("------------");
+                foreach(var student in Context.Students)
+                {
+                    Console.WriteLine(student.FirstMidName + student.LastName);
+                }
+
+                Console.WriteLine(string.Empty);
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
---
+++ testapps/ContosoUniversityBackendService/Program.cs
@@ -0,0 +1,69 @@
+﻿using ContosoUniversityBackendService.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ContosoUniversityBackendService
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    // context.Database.EnsureCreated();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+
+#if DEBUG
+            builder.AddJsonFile("appsettings.Development.json", true);
+#endif
+
+            var configuration = builder.Build();
+
+            var hostBuilder = Host.CreateDefaultBuilder()
+                                    .ConfigureServices(services =>
+                                    {
+                                        services.AddSingleton<IConfiguration>(builder.Build());
+                                        services.AddDbContext<SchoolContext>(options =>
+                                        {
+                                            options.UseSqlServer(configuration.GetConnectionString("SchoolContext"));
+                                        });
+                                        services.AddHostedService<ContosoService>();
+                                    });
+
+
+            return hostBuilder;
+        }
+    }
+}
