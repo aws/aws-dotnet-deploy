6dc380c88057c61949352f208e5bea70bae7e2de
Norm Johanson <normj@amazon.com>
2021-03-15 16:51:34 -0700

    Add ability for recipes to define a deployment confirmation message.

---
+++ src/AWS.Deploy.Common/Recipes/DeploymentConfirmationType.cs
@@ -0,0 +1,14 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWS.Deploy.Common.Recipes
+{
+    public class DeploymentConfirmationType
+    {
+        public string DefaultMessage { get; set; }
+    }
+}
