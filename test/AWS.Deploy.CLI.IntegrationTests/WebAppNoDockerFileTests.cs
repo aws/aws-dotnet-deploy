184be1ab031e7040ce036f54e40f82b7f956f6a2
Ganesh Jangiir <jangirg@amazon.com>
2021-04-21 10:05:16 -0700

    feat: add integration tests for Blazor, ECS Fargate (Service & Scheduled Task) and WebApps (Beanstalk and ECS Fargate Service)
    This changes adds automated integration tests that performs end to end testing for all five supported recipes with default configurations. A typical test case starts with deployment and ends at deleting the application.
    
    At the end of the test, regardless of the outcome, test clears the resources created by implementing IDisposable.
    
    `InMemoryInteractiveService` allows tests to capture stdout/stderr and provide stdin using Streams.

---
+++ test/AWS.Deploy.CLI.IntegrationTests/WebAppNoDockerFileTests.cs
@@ -0,0 +1,117 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.CloudFormation;
+using AWS.Deploy.CLI.Extensions;
+using AWS.Deploy.CLI.IntegrationTests.Extensions;
+using AWS.Deploy.CLI.IntegrationTests.Helpers;
+using AWS.Deploy.CLI.IntegrationTests.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Environment = System.Environment;
+
+namespace AWS.Deploy.CLI.IntegrationTests
+{
+    [Collection("Serial")]
+    public class WebAppNoDockerFileTests : IDisposable
+    {
+        private readonly HttpHelper _httpHelper;
+        private readonly CloudFormationHelper _cloudFormationHelper;
+        private readonly App _app;
+        private readonly InMemoryInteractiveService _interactiveService;
+        private bool _isDisposed;
+        private string _stackName;
+
+        public WebAppNoDockerFileTests()
+        {
+            _httpHelper = new HttpHelper();
+
+            var cloudFormationClient = new AmazonCloudFormationClient();
+            _cloudFormationHelper = new CloudFormationHelper(cloudFormationClient);
+
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddCustomServices();
+            serviceCollection.AddTestServices();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            _app = serviceProvider.GetService<App>();
+            Assert.NotNull(_app);
+
+            _interactiveService = serviceProvider.GetService<InMemoryInteractiveService>();
+            Assert.NotNull(_interactiveService);
+        }
+
+        [Fact]
+        public async Task DefaultConfigurations()
+        {
+            _stackName = $"WebAppNoDockerFile{Guid.NewGuid().ToString().Split('-').Last()}";
+
+            // Arrange input for deploy
+            await _interactiveService.StdInWriter.WriteAsync(Environment.NewLine); // Select default recommendation
+            await _interactiveService.StdInWriter.WriteAsync(Environment.NewLine); // Select default option settings
+            await _interactiveService.StdInWriter.FlushAsync();
+
+            // Deploy
+            var projectPath = Path.Combine("testapps", "WebAppNoDockerFile", "WebAppNoDockerFile.csproj");
+            var deployArgs = new[] { "deploy", "--project-path", projectPath, "--stack-name", _stackName };
+            await _app.Run(deployArgs);
+
+            // Verify application is deployed and running
+            Assert.Equal(StackStatus.CREATE_COMPLETE, await _cloudFormationHelper.GetStackStatus(_stackName));
+
+            var deployStdOut = _interactiveService.StdOutReader.ReadAllLines();
+
+            // Example: WebAppNoDockerFile-3cf258f103d2.EndpointURL = http://52.36.216.238/
+            var applicationUrl = deployStdOut.First(line => line.StartsWith($"{_stackName}.EndpointURL"))
+                .Split("=")[1]
+                .Trim();
+
+            // URL could take few more minutes to come live, therefore, we want to wait and keep trying for a specified timeout
+            await _httpHelper.WaitUntilSuccessStatusCode(applicationUrl, TimeSpan.FromSeconds(5), TimeSpan.FromMinutes(5));
+
+            // Arrange input for delete
+            await _interactiveService.StdInWriter.WriteAsync("y"); // Confirm delete
+            await _interactiveService.StdInWriter.FlushAsync();
+            var deleteArgs = new[] { "delete-deployment", _stackName };
+
+            // Delete
+            await _app.Run(deleteArgs);
+
+            // Verify application is delete
+            Assert.True(await _cloudFormationHelper.IsStackDeleted(_stackName));
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_isDisposed) return;
+
+            if (disposing)
+            {
+                var isStackDeleted = _cloudFormationHelper.IsStackDeleted(_stackName).GetAwaiter().GetResult();
+                if (!isStackDeleted)
+                {
+                    _cloudFormationHelper.DeleteStack(_stackName).GetAwaiter().GetResult();
+                }
+            }
+
+            _isDisposed = true;
+        }
+
+        ~WebAppNoDockerFileTests()
+        {
+            Dispose(false);
+        }
+    }
+}
