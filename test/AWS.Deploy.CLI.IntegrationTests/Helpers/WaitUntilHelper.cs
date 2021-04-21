9bb0741f618d55b1336e7241975c98026e0ba821
Ganesh Jangiir <jangirg@amazon.com>
2021-04-21 10:05:16 -0700

    test: add integration tests for Blazor, ECS Fargate (Service & Scheduled Task) and WebApps (Beanstalk and ECS Fargate Service)
    This changes adds automated integration tests that performs end to end testing for all five supported recipes with default configurations. A typical test case starts with deployment and ends at deleting the application.
    
    At the end of the test, regardless of the outcome, test clears the resources created by implementing IDisposable.
    
    `InMemoryInteractiveService` allows tests to capture stdout/stderr and provide stdin using Streams.

---
+++ test/AWS.Deploy.CLI.IntegrationTests/Helpers/WaitUntilHelper.cs
@@ -0,0 +1,34 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using System.Threading.Tasks;
+
+namespace AWS.Deploy.CLI.IntegrationTests.Helpers
+{
+    public static class WaitUntilHelper
+    {
+        /// <summary>
+        /// Wait for <see cref="timeout"/> TimeSpan until <see cref="Predicate{T}"/> isn't satisfied
+        /// </summary>
+        /// <param name="predicate">Termination condition for breaking the wait loop</param>
+        /// <param name="frequency">Interval between the two executions of the task</param>
+        /// <param name="timeout">Interval for timeout, if timeout passes, methods throws <see cref="TimeoutException"/></param>
+        /// <exception cref="TimeoutException">Throws when timeout passes</exception>
+        public static async Task WaitUntil(Func<Task<bool>> predicate, TimeSpan frequency, TimeSpan timeout)
+        {
+            var waitTask = Task.Run(async () =>
+            {
+                while (!await predicate())
+                {
+                    await Task.Delay(frequency);
+                }
+            });
+
+            if (waitTask != await Task.WhenAny(waitTask, Task.Delay(timeout)))
+            {
+                throw new TimeoutException();
+            }
+        }
+    }
+}
