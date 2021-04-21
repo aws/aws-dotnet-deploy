27e39969b8d2a03d6bc505f435b30bf205ac0d9b
Ganesh Jangiir <jangirg@amazon.com>
2021-04-21 10:05:16 -0700

    feat: add integration tests for Blazor, ECS Fargate (Service & Scheduled Task) and WebApps (Beanstalk and ECS Fargate Service)
    This changes adds automated integration tests that performs end to end testing for all five supported recipes with default configurations. A typical test case starts with deployment and ends at deleting the application.
    
    At the end of the test, regardless of the outcome, test clears the resources created by implementing IDisposable.
    
    `InMemoryInteractiveService` allows tests to capture stdout/stderr and provide stdin using Streams.

---
+++ test/AWS.Deploy.CLI.IntegrationTests/Helpers/CloudFormationHelper.cs
@@ -0,0 +1,61 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using System.Threading.Tasks;
+using Amazon.CloudFormation;
+using Amazon.CloudFormation.Model;
+using Xunit;
+
+namespace AWS.Deploy.CLI.IntegrationTests.Helpers
+{
+    public class CloudFormationHelper
+    {
+        private readonly IAmazonCloudFormation _cloudFormationClient;
+
+        public CloudFormationHelper(IAmazonCloudFormation cloudFormationClient)
+        {
+            _cloudFormationClient = cloudFormationClient;
+        }
+
+        public async Task<StackStatus> GetStackStatus(string stackName)
+        {
+            var stack = await GetStackAsync(stackName);
+            return stack.StackStatus;
+        }
+
+        public async Task<bool> IsStackDeleted(string stackName)
+        {
+            try
+            {
+                await GetStackAsync(stackName);
+            }
+            catch (AmazonCloudFormationException cloudFormationException) when (cloudFormationException.Message.Equals($"Stack with id {stackName} does not exist"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task DeleteStack(string stackName)
+        {
+            var request = new DeleteStackRequest()
+            {
+                StackName = stackName
+            };
+
+            await _cloudFormationClient.DeleteStackAsync(request);
+        }
+
+        private async Task<Stack> GetStackAsync(string stackName)
+        {
+            var response = await _cloudFormationClient.DescribeStacksAsync(new DescribeStacksRequest
+            {
+                StackName = stackName
+            });
+
+            return response.Stacks.Count == 0 ? null : response.Stacks[0];
+        }
+    }
+}
---
+++ test/AWS.Deploy.CLI.IntegrationTests/Helpers/CloudWatchLogsHelper.cs
@@ -0,0 +1,66 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.CloudWatchLogs;
+using Amazon.CloudWatchLogs.Model;
+
+namespace AWS.Deploy.CLI.IntegrationTests.Helpers
+{
+    public class CloudWatchLogsHelper
+    {
+        private readonly IAmazonCloudWatchLogs _client;
+
+        public CloudWatchLogsHelper(IAmazonCloudWatchLogs client)
+        {
+            _client = client;
+        }
+
+        public async Task<IEnumerable<string>> GetLogMessages(string logGroup)
+        {
+            var logStream = await GetLatestLogStream(logGroup);
+            return await GetLogMessages(logGroup, logStream);
+        }
+
+        private async Task<IEnumerable<string>> GetLogMessages(string logGroup, string logStreamName)
+        {
+            var request = new GetLogEventsRequest
+            {
+                LogGroupName = logGroup,
+                LogStreamName = logStreamName
+            };
+            ;
+            var logMessages = new List<string>();
+
+            var listStacksPaginator = _client.Paginators.GetLogEvents(request);
+            await foreach (var response in listStacksPaginator.Responses)
+            {
+                // When there are no new events, Events list is returned empty.
+                // Therefore, we want to stop the paginator and return the response.
+                // Otherwise, paginator will end up in an infinite loop.
+                if (response.Events.Count == 0)
+                {
+                    break;
+                }
+
+                var messages = response.Events.Select(e => e.Message);
+                logMessages.AddRange(messages);
+            }
+
+            return logMessages;
+        }
+
+        private async Task<string> GetLatestLogStream(string logGroup)
+        {
+            var request = new DescribeLogStreamsRequest
+            {
+                LogGroupName = logGroup
+            };
+
+            var response = await _client.DescribeLogStreamsAsync(request);
+            return response.LogStreams.FirstOrDefault()?.LogStreamName;
+        }
+    }
+}
