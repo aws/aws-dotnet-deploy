1e6653ebdd7d919d0804144f1902421460cdc9d2
Philippe El Asmar <phil.asmar@gmail.com>
2021-05-20 11:58:56 -0400

    feat: implement ServerModeSession utility class
    ServerModeSession exposes Start method which tries to start the deployment tools server on a given port range for the current process.

---
+++ src/AWS.Deploy.ServerMode.Client/Exceptions.cs
@@ -0,0 +1,27 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.\r
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+
+namespace AWS.Deploy.ServerMode.Client
+{
+    /// <summary>
+    /// Exception is thrown when deployment tool server failed to start for an unknown reason.
+    /// </summary>
+    public class InternalServerModeException : Exception
+    {
+        public InternalServerModeException(string message) : base(message)
+        {
+        }
+    }
+
+    /// <summary>
+    /// Exception is thrown when deployment tool server failed to start due to unavailability of free ports.
+    /// </summary>
+    public class PortUnavailableException : Exception
+    {
+        public PortUnavailableException(string message) : base(message)
+        {
+        }
+    }
+}
