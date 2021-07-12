371dee1e86a75d5de99131f5602634b6ce8c60d1
Philippe El Asmar <phil.asmar@gmail.com>
2021-07-12 14:42:29 -0700

    feat: add GetRecipe API to describe Recipes returned by other APIs

---
+++ src/AWS.Deploy.CLI/ServerMode/Controllers/RecipeController.cs
@@ -0,0 +1,51 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.\r
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Linq;
+using AWS.Deploy.CLI.ServerMode.Models;
+using AWS.Deploy.Orchestration;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace AWS.Deploy.CLI.ServerMode.Controllers
+{
+    [Produces("application/json")]
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class RecipeController : ControllerBase
+    {
+        /// <summary>
+        /// Gets a summary of the specified Recipe.
+        /// </summary>
+        [HttpGet("{recipeId}")]
+        [SwaggerOperation(OperationId = "GetRecipe")]
+        [SwaggerResponse(200, type: typeof(RecipeSummary))]
+        public IActionResult GetRecipe(string recipeId, [FromQuery] string? projectPath = null)
+        {
+            if (string.IsNullOrEmpty(recipeId))
+            {
+                return BadRequest($"A Recipe ID was not provided.");
+            }
+
+            var recipeDefinitions = RecipeHandler.GetRecipeDefinitions();
+            var selectedRecipeDefinition = recipeDefinitions.FirstOrDefault(x => x.Id.Equals(recipeId));
+
+            if (selectedRecipeDefinition == null)
+            {
+                return BadRequest($"Recipe ID {recipeId} not found.");
+            }
+
+            var output = new RecipeSummary(
+                selectedRecipeDefinition.Id,
+                selectedRecipeDefinition.Version,
+                selectedRecipeDefinition.Name,
+                selectedRecipeDefinition.Description,
+                selectedRecipeDefinition.TargetService,
+                selectedRecipeDefinition.DeploymentType.ToString(),
+                selectedRecipeDefinition.DeploymentBundle.ToString()
+            );
+
+            return Ok(output);
+        }
+    }
+}
--- test/AWS.Deploy.CLI.UnitTests/ServerModeTests.cs
+++ test/AWS.Deploy.CLI.UnitTests/ServerModeTests.cs
@@ -1,8 +1,13 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.\r
 // SPDX-License-Identifier: Apache-2.0
 
+using System.Linq;
 using System.Threading.Tasks;
 using AWS.Deploy.CLI.Commands;
+using AWS.Deploy.CLI.ServerMode.Controllers;
+using AWS.Deploy.CLI.ServerMode.Models;
+using AWS.Deploy.Orchestration;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
 namespace AWS.Deploy.CLI.UnitTests
@@ -29,6 +34,31 @@ namespace AWS.Deploy.CLI.UnitTests
             Assert.Single(serverModeTask2.Exception.InnerExceptions);
 
             Assert.IsType<TcpPortInUseException>(serverModeTask2.Exception.InnerException);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("InvalidId")]
+        public void RecipeController_GetRecipe_EmptyId(string recipeId)
+        {
+            var recipeController = new RecipeController();
+            var response = recipeController.GetRecipe(recipeId);
+
+            Assert.IsType<BadRequestObjectResult>(response);
+        }
+
+        [Fact]
+        public void RecipeController_GetRecipe_HappyPath()
+        {
+            var recipeController = new RecipeController();
+            var recipeDefinitions = RecipeHandler.GetRecipeDefinitions();
+            var recipe = recipeDefinitions.First();
+
+            var response = recipeController.GetRecipe(recipe.Id);
+
+            var result = Assert.IsType<OkObjectResult>(response);
+            var resultRecipe = Assert.IsType<RecipeSummary>(result.Value);
+            Assert.Equal(recipe.Id, resultRecipe.Id);
         }
     }
 }
