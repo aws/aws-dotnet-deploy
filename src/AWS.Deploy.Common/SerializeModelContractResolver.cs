ef81cc39bada999f6234690d2af0b3eea6475191
Norm Johanson <normj@amazon.com>
2021-08-19 16:12:54 -0700

    fix: Fix issue with TypeHintData not being serialized correctly when saving CDK project

--- src/AWS.Deploy.Common/SerializeModelContractResolver.cs
+++ src/AWS.Deploy.Common/SerializeModelContractResolver.cs
@@ -20,8 +20,23 @@ namespace AWS.Deploy.Common
             if (property != null && property.PropertyType != null && property.PropertyName != null && property.PropertyType != typeof(string))
             {
                 if (property.PropertyType.GetInterface(nameof(IEnumerable)) != null)
-                    property.ShouldSerialize =
-                        instance => (instance?.GetType()?.GetProperty(property.PropertyName)?.GetValue(instance) as IEnumerable<object>)?.Any() ?? false;
+                {
+                    property.ShouldSerialize = instance =>
+                    {
+                        var instanceValue = instance?.GetType()?.GetProperty(property.PropertyName)?.GetValue(instance);
+                        if (instanceValue is IEnumerable<object> list)
+                        {
+                            return list.Any();
+                        }
+                        else if(instanceValue is System.Collections.IDictionary map)
+                        {
+                            return map.Count > 0;
+                        }
+                            
+
+                        return false;
+                    };
+                }
             }
             return property ?? throw new ArgumentException();
         }
