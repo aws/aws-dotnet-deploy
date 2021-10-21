c6bde96f8dbc77fea3858a86490478b49c025baf
Norm Johanson <normj@amazon.com>
2021-10-20 17:21:49 -0700

    fix: Fixed issue where existing applications deployed from a deployment project can't be redeployed to.

--- test/AWS.Deploy.Orchestration.UnitTests/DeployedApplicationQueryerTests.cs
+++ test/AWS.Deploy.Orchestration.UnitTests/DeployedApplicationQueryerTests.cs
@@ -10,6 +10,7 @@ using Amazon.CloudFormation;
 using Amazon.CloudFormation.Model;
 using AWS.Deploy.Common;
 using AWS.Deploy.Common.IO;
+using AWS.Deploy.Common.Recipes;
 using AWS.Deploy.Orchestration.Data;
 using AWS.Deploy.Orchestration.LocalUserSettings;
 using AWS.Deploy.Orchestration.Utilities;
@@ -64,30 +65,116 @@ namespace AWS.Deploy.Orchestration.UnitTests
         }
 
         [Fact]
-        public async Task GetExistingDeployedApplications_DeployCall()
+        public async Task GetExistingDeployedApplications_CompatibleSystemRecipes()
         {
-            var stack = new Stack
-            {
-                Tags = new List<Tag>() { new Tag {
-                    Key = Constants.CloudFormationIdentifier.STACK_TAG,
-                    Value = "AspNetAppEcsFargate"
-                } },
-                Description = Constants.CloudFormationIdentifier.STACK_DESCRIPTION_PREFIX,
-                StackStatus = StackStatus.CREATE_COMPLETE,
-                StackName = "Stack1"
+            var stacks = new List<Stack> {
+                new Stack{
+                    Tags = new List<Tag>() { new Tag {
+                        Key = Constants.CloudFormationIdentifier.STACK_TAG,
+                        Value = "AspNetAppEcsFargate"
+                    } },
+                    Description = Constants.CloudFormationIdentifier.STACK_DESCRIPTION_PREFIX,
+                    StackStatus = StackStatus.CREATE_COMPLETE,
+                    StackName = "WebApp"
+                },
+                new Stack{
+                    Tags = new List<Tag>() { new Tag {
+                        Key = Constants.CloudFormationIdentifier.STACK_TAG,
+                        Value = "ConsoleAppEcsFargateService"
+                    } },
+                    Description = Constants.CloudFormationIdentifier.STACK_DESCRIPTION_PREFIX,
+                    StackStatus = StackStatus.CREATE_COMPLETE,
+                    StackName = "ServiceProcessor"
+                }
             };
 
             _mockAWSResourceQueryer
                 .Setup(x => x.GetCloudFormationStacks())
-                .Returns(Task.FromResult(new List<Stack>() { stack }));
+                .Returns(Task.FromResult(stacks));
 
             var deployedApplicationQueryer = new DeployedApplicationQueryer(
                 _mockAWSResourceQueryer.Object,
                 _mockLocalUserSettingsEngine.Object,
                 _mockOrchestratorInteractiveService.Object);
 
-            var result = await deployedApplicationQueryer.GetCompatibleApplications(new List<Recommendation>());
-            Assert.Empty(result);
+            var recommendations = new List<Recommendation>
+            {
+                new Recommendation(new RecipeDefinition("AspNetAppEcsFargate", "0.2.0",  "ASP.NET Core ECS", DeploymentTypes.CdkProject, DeploymentBundleTypes.Container, "", "", "", "", "" ), null, null, 100, new Dictionary<string, string>())
+                {
+
+                }
+            };
+
+
+            var result = await deployedApplicationQueryer.GetCompatibleApplications(recommendations);
+            Assert.Single(result);
+            Assert.Equal("AspNetAppEcsFargate", result[0].RecipeId);
+        }
+
+        [Fact]
+        public async Task GetExistingDeployedApplications_WithDeploymentProjects()
+        {
+            var stacks = new List<Stack> {
+                // Existing stack from the base recipe which should be valid
+                new Stack{
+                    Tags = new List<Tag>() { new Tag {
+                        Key = Constants.CloudFormationIdentifier.STACK_TAG,
+                        Value = "AspNetAppEcsFargate"
+                    } },
+                    Description = Constants.CloudFormationIdentifier.STACK_DESCRIPTION_PREFIX,
+                    StackStatus = StackStatus.CREATE_COMPLETE,
+                    StackName = "WebApp"
+                },
+                // Existing stack that was deployed custom deployment project. Should be valid.
+                new Stack{
+                    Tags = new List<Tag>() { new Tag {
+                        Key = Constants.CloudFormationIdentifier.STACK_TAG,
+                        Value = "AspNetAppEcsFargate-Custom"
+                    } },
+                    Description = Constants.CloudFormationIdentifier.STACK_DESCRIPTION_PREFIX,
+                    StackStatus = StackStatus.CREATE_COMPLETE,
+                    StackName = "WebApp-Custom"
+                },
+                // Stack created from a different recipe and should not be valid.
+                new Stack{
+                    Tags = new List<Tag>() { new Tag {
+                        Key = Constants.CloudFormationIdentifier.STACK_TAG,
+                        Value = "ConsoleAppEcsFargateService"
+                    } },
+                    Description = Constants.CloudFormationIdentifier.STACK_DESCRIPTION_PREFIX,
+                    StackStatus = StackStatus.CREATE_COMPLETE,
+                    StackName = "ServiceProcessor"
+                }
+            };
+
+            _mockAWSResourceQueryer
+                .Setup(x => x.GetCloudFormationStacks())
+                .Returns(Task.FromResult(stacks));
+
+            var deployedApplicationQueryer = new DeployedApplicationQueryer(
+                _mockAWSResourceQueryer.Object,
+                _mockLocalUserSettingsEngine.Object,
+                _mockOrchestratorInteractiveService.Object);
+
+            var recommendations = new List<Recommendation>
+            {
+                new Recommendation(new RecipeDefinition("AspNetAppEcsFargate-Custom", "0.2.0",  "Saved Deployment Project",
+                                                            DeploymentTypes.CdkProject, DeploymentBundleTypes.Container, "", "", "", "", "" )
+                                                            {
+                                                                PersistedDeploymentProject = true,
+                                                                BaseRecipeId = "AspNetAppEcsFargate"
+                                                            },
+                                                            null, null, 100, new Dictionary<string, string>())
+                {
+
+                }
+            };
+
+
+            var result = await deployedApplicationQueryer.GetCompatibleApplications(recommendations);
+            Assert.Equal(2, result.Count);
+            Assert.Contains(result, x => string.Equals("AspNetAppEcsFargate", x.RecipeId));
+            Assert.Contains(result, x => string.Equals("AspNetAppEcsFargate-Custom", x.RecipeId));
         }
 
         [Theory]
