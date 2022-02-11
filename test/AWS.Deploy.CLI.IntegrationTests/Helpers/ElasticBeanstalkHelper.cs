cbd161afc8af10be23d85da0a4a6547b57e2de35
Malhar Khimsaria <kmalhar@amazon.com>
2022-02-11 04:45:14 -0500

    fix: Correctly set deployment ResourceType during re-deployments via server mode

---
+++ test/AWS.Deploy.CLI.IntegrationTests/Helpers/ElasticBeanstalkHelper.cs
@@ -0,0 +1,200 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.ElasticBeanstalk;
+using Amazon.ElasticBeanstalk.Model;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using AWS.Deploy.Common.IO;
+using AWS.Deploy.Orchestration.Data;
+
+namespace AWS.Deploy.CLI.IntegrationTests.Helpers
+{
+    public class ElasticBeanstalkHelper
+    {
+        private readonly IAmazonElasticBeanstalk _client;
+        private readonly IAWSResourceQueryer _awsResourceQueryer;
+        private readonly IToolInteractiveService _interactiveService;
+
+        public ElasticBeanstalkHelper(IAmazonElasticBeanstalk client, IAWSResourceQueryer awsResourceQueryer, IToolInteractiveService toolInteractiveService)
+        {
+            _client = client;
+            _awsResourceQueryer = awsResourceQueryer;
+            _interactiveService = toolInteractiveService;
+        }
+
+        public async Task CreateApplicationAsync(string applicationName)
+        {
+            _interactiveService.WriteLine($"Create Elastic Beanstalk application: {applicationName}");
+            await _client.CreateApplicationAsync(new CreateApplicationRequest
+            {
+                ApplicationName = applicationName,
+                Description = "aws-dotnet-deploy-integ-test"
+            });
+        }
+
+        public async Task CreateApplicationVersionAsync(string applicationName, string versionLabel, string deploymentPackage)
+        {
+            _interactiveService.WriteLine($"Creating new application version for {applicationName}: {versionLabel}");
+
+            var bucketName = (await _client.CreateStorageLocationAsync()).S3Bucket;
+
+            var key = string.Format("{0}/AWSDeploymentArchive_{0}_{1}{2}",
+                                        applicationName.Replace(' ', '-'),
+                                        versionLabel.Replace(' ', '-'),
+                                        new FileManager().GetExtension(deploymentPackage));
+
+            await UploadToS3Async(bucketName, key, deploymentPackage);
+
+            await _client.CreateApplicationVersionAsync(new CreateApplicationVersionRequest
+            {
+                ApplicationName = applicationName,
+                VersionLabel = versionLabel,
+                SourceBundle = new S3Location { S3Bucket = bucketName, S3Key = key }
+            });
+        }
+
+        public async Task<bool> CreateEnvironmentAsync(string applicationName, string environmentName, string versionLabel)
+        {
+            _interactiveService.WriteLine($"Creating new Elastic Beanstalk environment {environmentName} with versionLabel {versionLabel}");
+
+            var startingEventDate = DateTime.Now;
+
+            await _client.CreateEnvironmentAsync(new CreateEnvironmentRequest
+            {
+                ApplicationName = applicationName,
+                EnvironmentName = environmentName,
+                VersionLabel = versionLabel,
+                PlatformArn = (await _awsResourceQueryer.GetLatestElasticBeanstalkPlatformArn()).PlatformArn,
+                OptionSettings = new List<ConfigurationOptionSetting>
+                {
+                    new ConfigurationOptionSetting("aws:autoscaling:launchconfiguration", "IamInstanceProfile", "aws-elasticbeanstalk-ec2-role"),
+                    new ConfigurationOptionSetting("aws:elasticbeanstalk:healthreporting:system", "SystemType", "basic")
+                }
+            });
+
+            return await WaitForEnvironmentCreateCompletion(applicationName, environmentName, startingEventDate);
+        }
+
+        public async Task<bool> DeleteApplication(string applicationName, string environmentName)
+        {
+            _interactiveService.WriteLine($"Deleting Elastic Beanstalk application: {applicationName}");
+            _interactiveService.WriteLine($"Deleting Elastic Beanstalk environment: {environmentName}");
+            await _client.DeleteApplicationAsync(new DeleteApplicationRequest
+            {
+                ApplicationName = applicationName,
+                TerminateEnvByForce = true
+            });
+
+            return await WaitForEnvironmentDeletion(environmentName);
+        }
+
+        public async Task<bool> VerifyEnvironmentVersionLabel(string environmentName, string expectedVersionLabel)
+        {
+            var envDescription = await _awsResourceQueryer.DescribeElasticBeanstalkEnvironment(environmentName);
+            _interactiveService.WriteLine($"The Elastic Beanstalk environment is pointing to \"{envDescription.VersionLabel}\" version label");
+            return string.Equals(envDescription.VersionLabel, expectedVersionLabel);
+        }
+
+        private async Task<bool> WaitForEnvironmentCreateCompletion(string applicationName, string environmentName, DateTime startingEventDate)
+        {
+            _interactiveService.WriteLine("Waiting for environment update to complete");
+
+            var success = true;
+            var environment = new EnvironmentDescription();
+            var lastEventDate = startingEventDate;
+            var requestEvents = new DescribeEventsRequest
+            {
+                ApplicationName = applicationName,
+                EnvironmentName = environmentName
+            };
+            var requestEnvironment = new DescribeEnvironmentsRequest
+            {
+                ApplicationName = applicationName,
+                EnvironmentNames = new List<string> { environmentName }
+            };
+
+            do
+            {
+                Thread.Sleep(5000);
+
+                var responseEnvironments = await _client.DescribeEnvironmentsAsync(requestEnvironment);
+                environment = responseEnvironments.Environments[0];
+
+                requestEvents.StartTimeUtc = lastEventDate;
+                var responseEvents = await _client.DescribeEventsAsync(requestEvents);
+                if (responseEvents.Events.Any())
+                {
+                    for (var i = responseEvents.Events.Count - 1; i >= 0; i--)
+                    {
+                        var evnt = responseEvents.Events[i];
+                        if (evnt.EventDate <= lastEventDate)
+                            continue;
+
+                        _interactiveService.WriteLine(evnt.EventDate.ToLocalTime() + "    " + evnt.Severity + "    " + evnt.Message);
+                        if (evnt.Severity == EventSeverity.ERROR || evnt.Severity == EventSeverity.FATAL)
+                        {
+                            success = false;
+                        }
+                    }
+
+                    lastEventDate = responseEvents.Events[0].EventDate;
+                }
+            } while (environment.Status == EnvironmentStatus.Launching || environment.Status == EnvironmentStatus.Updating);
+
+            return success;
+        }
+
+        private async Task<bool> WaitForEnvironmentDeletion(string environmentName)
+        {
+            var attemptCount = 0;
+            const int maxAttempts = 7;
+
+            while (attemptCount < maxAttempts)
+            {
+                attemptCount += 1;
+                var response = await _client.DescribeEnvironmentsAsync(new DescribeEnvironmentsRequest
+                {
+                    EnvironmentNames = new List<string> { environmentName }
+                });
+
+                if (!response.Environments.Any() || response.Environments.Single().Status == EnvironmentStatus.Terminated)
+                    return true;
+
+                await Task.Delay(GetWaitTime(attemptCount));
+            }
+
+            return false;
+        }
+
+        private TimeSpan GetWaitTime(int attemptCount)
+        {
+            var waitTime = Math.Pow(2, attemptCount) * 5;
+            return TimeSpan.FromSeconds(waitTime);
+        }
+
+        private async Task UploadToS3Async(string bucketName, string key, string filePath)
+        {
+            _interactiveService.WriteLine("Uploading application deployment package to S3...");
+
+            using (var stream =  File.OpenRead(filePath))
+            {
+                var request = new TransferUtilityUploadRequest()
+                {
+                    BucketName = bucketName,
+                    Key = key,
+                    InputStream = stream
+                };
+
+                var s3Client = new AmazonS3Client();
+                await new TransferUtility(s3Client).UploadAsync(request);
+            }
+        }
+    }
+}
---
+++ test/AWS.Deploy.CLI.IntegrationTests/Helpers/IAMHelper.cs
@@ -0,0 +1,99 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.IdentityManagement;
+using Amazon.IdentityManagement.Model;
+using AWS.Deploy.Orchestration.Data;
+
+namespace AWS.Deploy.CLI.IntegrationTests.Helpers
+{
+    public class IAMHelper
+    {
+        private readonly IAmazonIdentityManagementService _client;
+        private readonly IToolInteractiveService _interactiveService;
+        private readonly IAWSResourceQueryer _awsResourceQueryer;
+
+        public IAMHelper(IAmazonIdentityManagementService client, IAWSResourceQueryer awsResourceQueryer, IToolInteractiveService toolInteractiveService)
+        {
+            _client = client;
+            _awsResourceQueryer = awsResourceQueryer;
+            _interactiveService = toolInteractiveService;
+        }
+
+        public async Task CreateRoleForBeanstalkEnvionmentDeployment(string roleName)
+        {
+            _interactiveService.WriteLine($"Creating role {roleName} for deployment to Elastic Beanstalk environemnt");
+            var existingRoles = await _awsResourceQueryer.ListOfIAMRoles("ec2.amazonaws.com");
+            var role = existingRoles.FirstOrDefault(x => string.Equals(roleName, x.RoleName));
+            if (role != null)
+            {
+                _interactiveService.WriteLine($" The role {roleName} already exists");
+            }
+            else
+            {
+                var assumeRolepolicyDocument =
+               @"{
+                   'Version':'2008-10-17',
+                   'Statement':[
+                      {
+                         'Effect':'Allow',
+                         'Principal':{
+                            'Service':'ec2.amazonaws.com'
+                         },
+                         'Action':'sts:AssumeRole'
+                      }
+                   ]
+                }";
+
+                await _client.CreateRoleAsync(new CreateRoleRequest
+                {
+                    RoleName = roleName,
+                    AssumeRolePolicyDocument = assumeRolepolicyDocument.Replace("'", "\"")
+                });
+            }
+
+            InstanceProfile instanceProfile = null;
+            try
+            {
+                instanceProfile = (await _client.GetInstanceProfileAsync(new GetInstanceProfileRequest()
+                {
+                    InstanceProfileName = roleName
+                })).InstanceProfile;
+            }
+            catch (NoSuchEntityException) { }
+
+            // Check to see if an instance profile exists for this role and if not create it.
+            if (instanceProfile == null)
+            {
+                _interactiveService.WriteLine($"Creating new IAM Instance Profile {roleName}");
+                await _client.CreateInstanceProfileAsync(new CreateInstanceProfileRequest()
+                {
+                    InstanceProfileName = roleName
+                });
+
+                _interactiveService.WriteLine($"Attaching IAM role {roleName} to Instance Profile {roleName}");
+                await _client.AddRoleToInstanceProfileAsync(new AddRoleToInstanceProfileRequest()
+                {
+                    RoleName = roleName,
+                    InstanceProfileName = roleName
+                });
+            }
+            // If it already exists see if this role is already assigned and if not assign it.
+            else
+            {
+                _interactiveService.WriteLine($"IAM Instance Profile {roleName} already exists");
+                if (instanceProfile.Roles.FirstOrDefault(x => x.RoleName == roleName) == null)
+                {
+                    _interactiveService.WriteLine($"Attaching IAM role {roleName} to Instance Profile {roleName}");
+                    await _client.AddRoleToInstanceProfileAsync(new AddRoleToInstanceProfileRequest()
+                    {
+                        RoleName = roleName,
+                        InstanceProfileName = roleName
+                    });
+                }
+            }
+        }
+    }
+}
