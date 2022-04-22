6b2b953bb89254814d9c6ee6a033803609f20be5
Philippe El Asmar <phil.asmar@gmail.com>
2022-04-22 13:42:52 -0400

    chore: remove banner to notify of NuGet package name change

--- src/AWS.Deploy.CLI/App.cs
+++ src/AWS.Deploy.CLI/App.cs
@@ -31,12 +31,6 @@ namespace AWS.Deploy.CLI
             _toolInteractiveService.WriteLine("AWS .NET deployment tool for deploying .NET Core applications to AWS.");
             _toolInteractiveService.WriteLine("Project Home: https://github.com/aws/aws-dotnet-deploy");
             _toolInteractiveService.WriteLine(string.Empty);
-            _toolInteractiveService.WriteLine("---------------------------------------------------------------------");
-            _toolInteractiveService.WriteLine("Deprecation Notice: The name of the AWS .NET deployment tool NuGet package will change from 'AWS.Deploy.CLI' to 'AWS.Deploy.Tools'. " +
-                "In order to keep receiving updates, make sure to uninstall the current dotnet tool 'AWS.Deploy.CLI' and install 'AWS.Deploy.Tools'. " +
-                "The NuGet package 'AWS.Deploy.CLI' will no longer receive any updates, so please make sure to install the new package 'AWS.Deploy.Tools'.");
-            _toolInteractiveService.WriteLine("---------------------------------------------------------------------");
-            _toolInteractiveService.WriteLine(string.Empty);
 
             // if user didn't specify a command, default to help
             if (args.Length == 0)
