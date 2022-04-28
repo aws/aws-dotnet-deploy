9a52381e060b0323a4daa434a19bb2ec431390ae
Norm Johanson <normj@amazon.com>
2022-04-27 22:50:11 -0700

    fix: Consolidate the separate CLI and ServerMode CreateDeploymentBundle methods into the Orchestrator

--- src/AWS.Deploy.CLI/ServerMode/Tasks/DeployRecommendationTask.cs
+++ src/AWS.Deploy.CLI/ServerMode/Tasks/DeployRecommendationTask.cs
@@ -3,11 +3,9 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using AWS.Deploy.Common;
-using AWS.Deploy.Common.IO;
 using AWS.Deploy.Common.Recipes;
 using AWS.Deploy.Orchestration;
 
@@ -19,20 +17,18 @@ namespace AWS.Deploy.CLI.ServerMode.Tasks
         private readonly Orchestrator _orchestrator;
         private readonly OrchestratorSession _orchestratorSession;
         private readonly Recommendation _selectedRecommendation;
-        private readonly IDirectoryManager _directoryManager;
 
-        public DeployRecommendationTask(OrchestratorSession orchestratorSession, Orchestrator orchestrator, CloudApplication cloudApplication, Recommendation selectedRecommendation, IDirectoryManager directoryManager)
+        public DeployRecommendationTask(OrchestratorSession orchestratorSession, Orchestrator orchestrator, CloudApplication cloudApplication, Recommendation selectedRecommendation)
         {
             _orchestratorSession = orchestratorSession;
             _orchestrator = orchestrator;
             _cloudApplication = cloudApplication;
             _selectedRecommendation = selectedRecommendation;
-            _directoryManager = directoryManager;
         }
 
         public async Task Execute()
         {
-            await CreateDeploymentBundle();
+            await _orchestrator.CreateDeploymentBundle(_cloudApplication, _selectedRecommendation);
             await _orchestrator.DeployRecommendation(_cloudApplication, _selectedRecommendation);
         }
 
@@ -46,7 +42,8 @@ namespace AWS.Deploy.CLI.ServerMode.Tasks
             if (cdkProjectHandler == null)
                 throw new FailedToCreateCDKProjectException(DeployToolErrorCode.FailedToCreateCDKProject, $"We could not create a CDK deployment project due to a missing dependency '{nameof(cdkProjectHandler)}'.");
 
-            await CreateDeploymentBundle();
+            await _orchestrator.CreateDeploymentBundle(_cloudApplication, _selectedRecommendation);
+
             var cdkProject = await cdkProjectHandler.ConfigureCdkProject(_orchestratorSession, _cloudApplication, _selectedRecommendation);
             try
             {
@@ -55,33 +52,6 @@ namespace AWS.Deploy.CLI.ServerMode.Tasks
             finally
             {
                 cdkProjectHandler.DeleteTemporaryCdkProject(cdkProject);
-            }
-        }
-
-        private async Task CreateDeploymentBundle()
-        {
-            if (_selectedRecommendation.Recipe.DeploymentBundle == DeploymentBundleTypes.Container)
-            {
-                var dockerBuildDeploymentBundleResult = await _orchestrator.CreateContainerDeploymentBundle(_cloudApplication, _selectedRecommendation);
-                if (!dockerBuildDeploymentBundleResult)
-                {
-                    if (!_selectedRecommendation.ProjectDefinition.HasDockerFile)
-                    {
-                        var projectDirectory = _directoryManager.GetDirectoryInfo(_selectedRecommendation.ProjectPath).Parent.FullName;
-                        var dockerfilePath = Path.Combine(projectDirectory, "Dockerfile");
-                        var errorMessage = $"Failed to create a container image from generated Docker file. " +
-                            $"Please edit the Dockerfile at {dockerfilePath} to correct the required build steps for the project. Common errors are missing project dependencies not included in the Dockerfile.";
-
-                        throw new FailedToCreateDeploymentBundleException(DeployToolErrorCode.FailedToCreateContainerDeploymentBundleFromGeneratedDockerFile, errorMessage);
-                    }
-                    throw new FailedToCreateDeploymentBundleException(DeployToolErrorCode.FailedToCreateContainerDeploymentBundle, "Failed to create a deployment bundle");
-                }
-            }
-            else if (_selectedRecommendation.Recipe.DeploymentBundle == DeploymentBundleTypes.DotnetPublishZipFile)
-            {
-                var dotnetPublishDeploymentBundleResult = await _orchestrator.CreateDotnetPublishDeploymentBundle(_selectedRecommendation);
-                if (!dotnetPublishDeploymentBundleResult)
-                    throw new FailedToCreateDeploymentBundleException(DeployToolErrorCode.FailedToCreateDotnetPublishDeploymentBundle, "Failed to create a deployment bundle");
             }
         }
     }
